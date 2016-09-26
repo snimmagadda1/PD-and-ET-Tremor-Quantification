d8bfe86b8d8d5ef900d19b7d50c1f442c36c3caf
Sai Nimmagadda <snn7@duke.edu>
2016-09-26 10:25:27 -0400

    testing binding functions

--- Tkinter/tkinter_test.py
+++ Tkinter/tkinter_test.py
@@ -8,14 +8,16 @@ if __name__ == "__main__":
             frame = Frame(master)
             frame.pack()
 
-            self.button = Button(
-                frame, text="QUIT", fg="red", command=frame.quit
-            )
-            self.button.pack(side=LEFT)
+            # make two button objects belonging to App
 
+            # associate two functions with buttons
             self.hi_there = Button(frame, text="Hello", command=self.say_hi)
             self.hi_there.pack(side=LEFT)
 
+            self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+            self.button.pack(side=LEFT)
+
+            # define function
         def say_hi(self):
             print("Hello Friends")
 
