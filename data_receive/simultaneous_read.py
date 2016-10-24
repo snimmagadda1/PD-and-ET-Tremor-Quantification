3585aa325198c02df816f5b7c1821dc1095e01b6
Sai Nimmagadda <snn7@duke.edu>
2016-10-24 11:20:20 -0400

    updating multiprocessing with datetime

--- data_receive/simultaneous_read.py
+++ data_receive/simultaneous_read.py
@@ -1,6 +1,10 @@
 def read_csv():
     import csv
-    print("READING CSV")
+    import datetime
+
+    datetime.datetime.now().time()
+    print(str(datetime.datetime.now().time()) + " READING CSV")
+
     with open('output.csv', 'r', newline='') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -9,6 +13,10 @@ def read_csv():
 
 def write_csv():
     import csv, random
+    import datetime
+
+    datetime.datetime.now().time()
+    print(str(datetime.datetime.now().time()) + " WRITING CSV")
 
     RESULTS1 = random.sample(range(1, 10000), 1000)
     RESULTS2 = random.sample(range(1, 10000), 1000)
@@ -19,11 +27,27 @@ def write_csv():
     wr.writerow(RESULTS2)
     wr.writerow(RESULTS3)
 
+
+from time import sleep
+import subprocess
+import threading
+
+
+def runmodels(arg):
+
+    subprocess.call(arg, shell=True)
+    sGlobal.release() # release for next launch
+
 if __name__ == "__main__":
     from multiprocessing import Process
+    import time
+
+
 
     p1 = Process(target=write_csv())
     p1.start()
+    #time.sleep(10)
     p2 = Process(target=read_csv())
     p2.start()
+
 
