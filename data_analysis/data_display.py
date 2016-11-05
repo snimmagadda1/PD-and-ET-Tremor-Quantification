6e07705df2d0a493b14c7695dcb2a27815b54fb3
Sai Nimmagadda <snn7@duke.edu>
2016-11-05 17:13:35 -0400

    fixing package data testing function

--- data_analysis/data_display.py
+++ data_analysis/data_display.py
@@ -13,13 +13,13 @@ def test_14hz_sampling():
     from process_data import remove_gravity_ENMO, \
         calculate_magnitude_acceleration,\
         butter_lowpass_filter, integrate_time_series, gs_to_accel
-    from package_data import extrapolate_accel_data
+    from package_data import extrapolate_accel_data_testing
 
     import numpy as np
     import matplotlib.pyplot as plt
 
     fs = 115
-    x_g, y_g, z_g = extrapolate_accel_data('sinusoid_14hz_fs_115.txt')
+    x_g, y_g, z_g = extrapolate_accel_data_testing('sinusoid_14hz_fs_115.txt')
     x_accel, y_accel, z_accel = gs_to_accel(x_g, y_g, z_g)
 
     # remove high frequencies
@@ -67,13 +67,13 @@ def test_8hz_sampling():
     from process_data import remove_gravity_ENMO, \
         calculate_magnitude_acceleration,\
         butter_lowpass_filter, integrate_time_series, gs_to_accel
-    from package_data import extrapolate_accel_data
+    from package_data import extrapolate_accel_data_testing
 
     import numpy as np
     import matplotlib.pyplot as plt
 
     fs = 115
-    x_g, y_g, z_g = extrapolate_accel_data('sinusoid_8hz_fs_115.txt')
+    x_g, y_g, z_g = extrapolate_accel_data_testing('sinusoid_8hz_fs_115.txt')
     x_accel, y_accel, z_accel = gs_to_accel(x_g, y_g, z_g)
 
     # remove high frequencies
@@ -121,13 +121,13 @@ def test_2hz_sampling():
     from process_data import remove_gravity_ENMO, \
         calculate_magnitude_acceleration,\
         butter_lowpass_filter, integrate_time_series, gs_to_accel
-    from package_data import extrapolate_accel_data
+    from package_data import extrapolate_accel_data_testing
 
     import numpy as np
     import matplotlib.pyplot as plt
 
     fs = 115
-    x_g, y_g, z_g = extrapolate_accel_data('sinusoid_2hz_fs_115.txt')
+    x_g, y_g, z_g = extrapolate_accel_data_testing('sinusoid_2hz_fs_115.txt')
     x_accel, y_accel, z_accel = gs_to_accel(x_g, y_g, z_g)
 
     # remove high frequencies
@@ -175,13 +175,13 @@ def test_0hz_sampling():
     from process_data import remove_gravity_ENMO, \
         calculate_magnitude_acceleration,\
         butter_lowpass_filter, integrate_time_series, gs_to_accel
-    from package_data import extrapolate_accel_data
+    from package_data import extrapolate_accel_data_testing
 
     import numpy as np
     import matplotlib.pyplot as plt
 
     fs = 115
-    x_g, y_g, z_g = extrapolate_accel_data('sinusoid_0hz_fs_115.txt')
+    x_g, y_g, z_g = extrapolate_accel_data_testing('sinusoid_0hz_fs_115.txt')
 
     # remove high frequencies
     x_filt = butter_lowpass_filter(x_g,  15, 44)
@@ -226,13 +226,13 @@ def display_integrations():
     from process_data import remove_gravity_ENMO, \
         calculate_magnitude_acceleration, \
         butter_lowpass_filter, integrate_time_series, gs_to_accel
-    from package_data import extrapolate_accel_data
+    from package_data import extrapolate_accel_data_testing
 
     import numpy as np
     import matplotlib.pyplot as plt
 
     fs = 115
-    x_g, y_g, z_g = extrapolate_accel_data('sinusoid_8hz_fs_115.txt')
+    x_g, y_g, z_g = extrapolate_accel_data_testing('sinusoid_8hz_fs_115.txt')
     x_accel, y_accel, z_accel = gs_to_accel(x_g, y_g, z_g)
 
     # remove high frequencies
@@ -290,50 +290,8 @@ def troubleshoot_integrations():
     plt.show()
 
 
-def test_remove_artifacts():
-    from process_data import remove_gravity_ENMO, \
-        calculate_magnitude_acceleration, \
-        butter_lowpass_filter, integrate_time_series, gs_to_accel
-    from package_data import extrapolate_accel_data
-
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    fs = 14
-    x_g, y_g, z_g = extrapolate_accel_data('rawacceldata.txt')
-    x_accel, y_accel, z_accel = gs_to_accel(x_g, y_g, z_g)
-
-    # remove high frequencies
-    x_filt = butter_lowpass_filter(x_accel, 15, 44)
-    y_filt = butter_lowpass_filter(y_accel, 15, 44)
-    z_filt = butter_lowpass_filter(z_accel, 15, 44)
-    time = np.arange(0, len(x_filt), 1) / float(fs)
-
-    # calculate magnitude of acceleration with and without grav (filtered)
-    acceleration_raw = calculate_magnitude_acceleration(x_accel, y_accel, z_accel)
-    acceleration_raw_no_grav = remove_gravity_ENMO(x_accel, y_accel, z_accel)
-
-    # calculate magnitude of acceleration with and without grav (filtered)
-    acceleration_filtered = calculate_magnitude_acceleration(x_filt, y_filt, z_filt)
-    acceleration_filtered_no_grav = remove_gravity_ENMO(x_filt, y_filt, z_filt)
-
-    f1 = plt.figure()
-    ax1 = f1.add_subplot(111)
-    ax1.plot(time, x_g, color='r')
-
-    f2 = plt.figure()
-    ax2 = f2.add_subplot(111)
-    ax2.plot(time, y_g, color='g')
-
-    f3 = plt.figure()
-    ax3 = f3.add_subplot(111)
-    ax3.plot(time, z_filt, color='g')
-
-    plt.show()
-
-
 if __name__ == "__main__":
-    test_remove_artifacts()
+    display_integrations()
 
     pass
 
--- data_analysis/package_data.py
+++ data_analysis/package_data.py
@@ -30,6 +30,33 @@ def extrapolate_accel_data(filename):
     return np.array(x), np.array(y), np.array(z)
 
 
+def extrapolate_accel_data_testing(filename):
+    """Extrapolate data from a txt file
+    data will have format:
+    x1,y1,z1;
+    x2,y2,z2;
+    ..
+    xn,yn,zn;
+
+    :param filename: file to read from
+    :return: datax, datay, dataz vectors (np.array)
+    """
+    x = []
+    y = []
+    z = []
+    import numpy as np
+    with open(filename, 'r') as f:
+        alllines = f.readlines()
+        for line in alllines:
+            components = line.split(',')
+            sep = ';'
+            cleaned_z = components[2].split(sep,1)[0]
+            components[2] = cleaned_z
+            x.append(float(components[0]))
+            y.append(float(components[1]))
+            z.append(float(components[2]))
+    return np.array(x), np.array(y), np.array(z)
+
 if __name__ == "__main__":
 
     pass
