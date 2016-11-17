0fd7114c59512df3030627108a0a0555ad594148
Sai Nimmagadda <snn7@duke.edu>
2016-11-17 13:07:48 -0500

    removing unused import

--- Tkinter/tk_GUI.py
+++ Tkinter/tk_GUI.py
@@ -1,7 +1,6 @@
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from matplotlib import style
