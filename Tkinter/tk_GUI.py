eb6f678114b7906ccfe29ce5f4e7cb807163205c
drw28 <drw28@duke.edu>
2016-11-17 15:33:14 -0500

    working on udprs, hex color

--- Tkinter/tk_GUI.py
+++ Tkinter/tk_GUI.py
@@ -29,6 +29,8 @@ LARGE_FONT = ("Verdana", 12)
 NORM_FONT = ("Verdana", 10)
 SMALL_FONT = ("Verdana", 8)
 
+MAIN_COLOR = "#5DBCD2"
+
 style.use("ggplot")
 
 # declare main plot and geometry here
@@ -223,107 +225,130 @@ class graph_page(tk.Frame):
 
 class updrs_page(tk.Frame):
 
+    speech_options = ["0 = Normal.",
+                      "1 = Slight loss of expression, diction and/or volume.",
+                      "2 = Monotone, slurred but understandable; moderately impaired.",
+                      "3 = Marked impairment, difficult to understand.",
+                      "4 = Unintelligible."]
+    facial_options = ["0 = Normal.",
+                      "1 = Minimal hypomimia, could be normal \"Poker Face\".",
+                      "2 = Slight but definitely abnormal diminution of facial expression.",
+                      "3 = Moderate hypomimia; lips parted some of the time.",
+                      "4 = Masked or fixed facies with severe or complete loss of facial expression; lips parted 1/4 inch or more."]
+    rigidity_options = ["0 = Absent.",
+                        "1 = Slight or detectable only when activated by mirror or other movements.",
+                        "2 = Mild to moderate.",
+                        "3 = Marked, but full range of motion easily achieved.",
+                        "4 = Severe, range of motion achieved with difficulty."]
+    hand_options = ["0 = Normal.",
+                    "1 = Mild slowing and/or reduction in amplitude.",
+                    "2 = Moderately impaired. Definite and early fatiguing. May have occasional arrests in movement.",
+                    "3 = Severely impaired. Frequent hesitation in initiating movements or arrests in ongoing movement.",
+                    "4 = Can barely perform the task."]
+
+    fingertaps_options = hand_options
+    rapidalternate_options = hand_options
+    legagility_options = hand_options
+    chair_options = ["0 = Normal",
+                     "1 = Slow; or may need more than one attempt.",
+                     "2 = Pushes self up from arms of seat.",
+                     "3 = Tends to fall back and may have to try ore than one time, but can get up without help.",
+                     "4 = Unable to arise without help."]
+    posture_options = ["0 = Normal erect.",
+                       "1 = Not quite erect, slightly stooped in posture; could be normal for an older person.",
+                       "2 = Moderately stooped in posture, definitely abnormal; can be slightly leaning to one side.",
+                       "3 = Severely stooped posture with kyphosis; can be moderately leaning to one side.",
+                       "4 = Marked flexion with extreme abnormality of posture."]
+    gait_options = ["0 = Normal.",
+                    "1 = Walks slowly, may shuffle with short steps, but no festination (hastening steps) or propulsion.",
+                    "2 = Walks with difficulty, but requires little or no assistance; may have some festination, short steps, or propulsion.",
+                    "3 = Severe disturbance of gait, requiring assistance.",
+                    "4 = Cannot walk at all, even with assistance."]
+    posturalstability_options = ["0 = Normal.",
+                                 "1 = Retropulsion, but recovers unaided.",
+                                 "2 = Absence of postural response; would fall if not caught by examiner.",
+                                 "3 = Very unstable, tends to lose balance spontaneously.",
+                                 "4 = Unable to stand without assistance."]
+    bodykinesia_options = ["0 = None.",
+                           "1 = Minimal slowness, giving movement a deliberate character; could be normal for some persons. Possibly reduced amplitude.",
+                           "2 = Mild degree of slowness and poverty of movement which is definitely abnormal. Alternatively some reduced amplitude.",
+                           "3 = Moderate slowness, poverty or small amplitude of movement.",
+                           "4 = Marked slowness, poverty or small amplitude of movement."]
+
+    all_options = [speech_options,
+                   facial_options,
+                   rigidity_options,
+                   hand_options,
+                   fingertaps_options,
+                   rapidalternate_options,
+                   legagility_options,
+                   chair_options,
+                   posture_options,
+                   gait_options,
+                   posturalstability_options,
+                   bodykinesia_options]
+    all_titles = ["Speech",
+                  "Facial Expression",
+                  "Rigidity",
+                  "Finger Taps",
+                  "Hand Movements",
+                  "Rapid Alternating Movement of Hands",
+                  "Leg Agility",
+                  "Arising from Chair",
+                  "Posture",
+                  "Gait",
+                  "Postural Stability",
+                  "Body Bradykinesia and Hypokinesia"]
+
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
-        title = tk.Label(self, text="Unified Parkinsons Disease Rating Scale", font=TITLE_FONT)
+        self.pack()
+
+        self.title_frame(self).pack()
+
+        # self.misc_options_frame(parent)
+        # self.tremor_rest_frame(parent)
+        # self.tremor_postural_frame(parent)
+        # self.rigidity_frame(parent)
+        # self.fingertaps_frame(parent)
+        # self.hand_movements_frame(parent)
+        # self.rapid_alternating_frame(parent)
+        # self.leg_agility_frame(parent)
+
+
+
+
+
+        # all_vars = []
+        # rownum = 1
+        # num_columns = 4
+        #
+        # for i in range(0, len(all_options), num_columns):
+        #     for c in range(num_columns):
+        #         # Label
+        #         label = tk.Label(self, text=all_titles[i+c])
+        #         label.grid(row=rownum, column=c, sticky="W")
+        #
+        #         # Menu
+        #         var = tk.StringVar()
+        #         var.set(all_options[i][0]) # set defaults
+        #         all_vars.append(var)
+        #
+        #         menuargs = (self, var, *all_options[i+c])
+        #         menu = tk.OptionMenu(*menuargs)
+        #         menu.grid(row=rownum+1, column=c, sticky="W")
+        #
+        #     rownum += 2
+
+    def title_frame(self, parent):
+        self.title_f = tk.Frame(parent, width = 1280, height = 100, bg=MAIN_COLOR)
+
+        title = tk.Label(self.title_f, text="Unified Parkinsons Disease Rating Scale", font=TITLE_FONT, bg=MAIN_COLOR)
         title.place(relx=0.5, rely=0.5, anchor='center')
-        title.grid(row=0, column=0, columnspan=2)
+        self.title_f.grid()
+        return self
 
-        speech_options = ["0 = Normal.",
-                          "1 = Slight loss of expression, diction and/or volume.",
-                          "2 = Monotone, slurred but understandable; moderately impaired.",
-                          "3 = Marked impairment, difficult to understand.",
-                          "4 = Unintelligible."]
-        facial_options = ["0 = Normal.",
-                          "1 = Minimal hypomimia, could be normal \"Poker Face\".",
-                          "2 = Slight but definitely abnormal diminution of facial expression.",
-                          "3 = Moderate hypomimia; lips parted some of the time.",
-                          "4 = Masked or fixed facies with severe or complete loss of facial expression; lips parted 1/4 inch or more."]
-        rigidity_options = ["0 = Absent.",
-                            "1 = Slight or detectable only when activated by mirror or other movements.",
-                            "2 = Mild to moderate.",
-                            "3 = Marked, but full range of motion easily achieved.",
-                            "4 = Severe, range of motion achieved with difficulty."]
-        hand_options = ["0 = Normal.",
-                        "1 = Mild slowing and/or reduction in amplitude.",
-                        "2 = Moderately impaired. Definite and early fatiguing. May have occasional arrests in movement.",
-                        "3 = Severely impaired. Frequent hesitation in initiating movements or arrests in ongoing movement.",
-                        "4 = Can barely perform the task."]
-
-        fingertaps_options = hand_options
-        rapidalternate_options = hand_options
-        legagility_options = hand_options
-        chair_options = ["0 = Normal",
-                         "1 = Slow; or may need more than one attempt.",
-                         "2 = Pushes self up from arms of seat.",
-                         "3 = Tends to fall back and may have to try ore than one time, but can get up without help.",
-                         "4 = Unable to arise without help."]
-        posture_options = ["0 = Normal erect.",
-                           "1 = Not quite erect, slightly stooped in posture; could be normal for an older person.",
-                           "2 = Moderately stooped in posture, definitely abnormal; can be slightly leaning to one side.",
-                           "3 = Severely stooped posture with kyphosis; can be moderately leaning to one side.",
-                           "4 = Marked flexion with extreme abnormality of posture."]
-        gait_options = ["0 = Normal.",
-                        "1 = Walks slowly, may shuffle with short steps, but no festination (hastening steps) or propulsion.",
-                        "2 = Walks with difficulty, but requires little or no assistance; may have some festination, short steps, or propulsion.",
-                        "3 = Severe disturbance of gait, requiring assistance.",
-                        "4 = Cannot walk at all, even with assistance."]
-        posturalstability_options = ["0 = Normal.",
-                                     "1 = Retropulsion, but recovers unaided.",
-                                     "2 = Absence of postural response; would fall if not caught by examiner.",
-                                     "3 = Very unstable, tends to lose balance spontaneously.",
-                                     "4 = Unable to stand without assistance."]
-        bodykinesia_options = ["0 = None.",
-                               "1 = Minimal slowness, giving movement a deliberate character; could be normal for some persons. Possibly reduced amplitude.",
-                               "2 = Mild degree of slowness and poverty of movement which is definitely abnormal. Alternatively some reduced amplitude.",
-                               "3 = Moderate slowness, poverty or small amplitude of movement.",
-                               "4 = Marked slowness, poverty or small amplitude of movement."]
-
-        all_options = [speech_options,
-                       facial_options,
-                       rigidity_options,
-                       hand_options,
-                       fingertaps_options,
-                       rapidalternate_options,
-                       legagility_options,
-                       chair_options,
-                       posture_options,
-                       gait_options,
-                       posturalstability_options,
-                       bodykinesia_options]
-        all_titles = ["Speech",
-                      "Facial Expression",
-                      "Rigidity",
-                      "Finger Taps",
-                      "Hand Movements",
-                      "Rapid Alternating Movement of Hands",
-                      "Leg Agility",
-                      "Arising from Chair",
-                      "Posture",
-                      "Gait",
-                      "Postural Stability",
-                      "Body Bradykinesia and Hypokinesia"]
-        all_vars = []
-        rownum = 1
-        num_columns = 4
-
-        for i in range(0, len(all_options), num_columns):
-            for c in range(num_columns):
-                # Label
-                label = tk.Label(self, text=all_titles[i+c])
-                label.grid(row=rownum, column=c, sticky="W")
-
-                # Menu
-                var = tk.StringVar()
-                var.set(all_options[i][0]) # set defaults
-                all_vars.append(var)
-
-                menuargs = (self, var, *all_options[i+c])
-                menu = tk.OptionMenu(*menuargs)
-                menu.grid(row=rownum+1, column=c, sticky="W")
-
-            rownum += 2
 
 
 
